fec467b3148f77e918ad09235b2a7045a0790543
YeeTing18 <140422584+YeeTing18@users.noreply.github.com>
2026-02-09 13:03:03 +0800

    Delete start-server.bat

--- start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Salary System Email Server...
-cd /d %~dp0
-title Salary System Email Server
-color 0A
-echo Server is starting... Please do not close this window!
-echo.
-node server.js
-pause 
